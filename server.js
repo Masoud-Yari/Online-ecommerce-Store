0e9aa49c56c5f6a7221f74fa4f7cb51a40eb3eef
Masoud-Yari <masoudd.yarii@gmail.com>
2020-09-20 15:02:24 +0430

    prepare for publish

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 dotenv.config();
 
@@ -12,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.set('useCreateIndex', true);
 
-const db = process.env.MONGODB_URL;
+const db = process.env.MONGODB_URL || 'mongodb://localhost/my-shop';
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, error => {
     if(error) 
         console.log(error);
@@ -25,5 +26,13 @@ app.use('/api/users', require('./routes/api/user'));
 
 
 const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
